6dbd8eefb0f9c386946771f824a4f713d8ac0f83
azam alias <azamalias@gmail.com>
2016-08-26 00:48:12 +0800

    added more tests, all zeroes fails though :(

--- Equi.Tests/SolutionTests.cs
+++ Equi.Tests/SolutionTests.cs
@@ -26,5 +26,26 @@ namespace Equi.Tests
             Assert.Equal(-1, result);
         }
 
+        [Fact]
+        public void ThreeElements_allZeroes_returnsIndex1()
+        {
+            var result = Solution.solution(new int[] { 0, 0, 0 });
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void ThreeElements_allOnes_returnsIndex1()
+        {
+            var result = Solution.solution(new int[] { 1, 1, 1 });
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void ThreeElements_allNegativeOnes_returnsIndex1()
+        {
+            var result = Solution.solution(new int[] { -1, -1, -1 });
+            Assert.Equal(1, result);
+        }
+
     }
 }
