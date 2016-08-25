8690d8ab80f423c40e15d20430c2cbc234ae136a
azam alias <azamalias@gmail.com>
2016-08-26 00:28:14 +0800

    first Equi implementation + test

---
+++ Equi.Tests/SolutionTests.cs
@@ -0,0 +1,15 @@
+﻿
+using Xunit;
+
+namespace Equi.Tests
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void ThreeElements_withMiddleIndex_returnsIndex1()
+        {
+            var result = Solution.solution(new int[]{5, 2, 5});
+            Assert.Equal(1, result);
+        }
+    }
+}
---
+++ Equi/Solution.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Equi
+{
+    public class Solution
+    {
+        public static int solution(int[] A)
+        {
+            var length = A.Length;
+            var totalSum = A.Sum();
+            var leftSum = 0;
+            var rightSum = 0;
+
+            for (int i=0; i<length; i++)
+            {
+                var currentElement = A[i];
+
+                rightSum = totalSum - leftSum - currentElement;
+                
+                if (leftSum == rightSum)
+                {
+                    return i;
+                }
+
+                leftSum =+ A[i];
+            }
+
+            return -1;
+        }
+    }
+}
