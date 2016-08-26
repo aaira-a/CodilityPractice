d5cdb69893f562142662dbf7ad14425ee2e5619d
azam alias <azamalias@gmail.com>
2016-08-27 02:59:53 +0800

    solved Task2. needs optimization and handling for extreme inputs

--- NXNCPR-X72-2.Tests/SolutionTests.cs
+++ NXNCPR-X72-2.Tests/SolutionTests.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace NXNCPR_X72_2.Tests
 {
     public class SolutionTests
     {
+        [Fact]
+        public void Test_28_returns_7()
+        {
+            var binaryString = Convert.ToString(28, 2);
+            var result = Solution.solution(binaryString);
+            Assert.Equal(7, result);
+        }
     }
 }
--- NXNCPR-X72-2/Solution.cs
+++ NXNCPR-X72-2/Solution.cs
@@ -8,5 +8,28 @@ namespace NXNCPR_X72_2
 {
     public class Solution
     {
+        public static int solution(string s)
+        {
+            var number = Convert.ToInt32(s, 2);
+
+            var steps = 0;
+
+            while (number > 0)
+            {
+                if (number %2 == 1)
+                {
+                    number -= 1;
+                    steps += 1;
+                }
+
+                if ((number !=0) && (number % 2 == 0))
+                {
+                    number /= 2;
+                    steps += 1;
+                }
+            }
+
+            return steps;
+        }
     }
 }
