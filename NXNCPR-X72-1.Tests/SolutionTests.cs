c3fe8ba1406e5d7885b10ae05b0cd2f226d3cadd
azam alias <azamalias@gmail.com>
2016-08-27 02:23:16 +0800

    solved Task1. needs refactoring badly

--- NXNCPR-X72-1.Tests/SolutionTests.cs
+++ NXNCPR-X72-1.Tests/SolutionTests.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace NXNCPR_X72_1.Tests
 {
     public class SolutionTests
     {
+        [Fact]
+        public void Test_12_returns_21()
+        {
+            var result = Solution.solution(12);
+            Assert.Equal(21, result);
+        }
+
+        [Fact]
+        public void Test_122_returns_221()
+        {
+            var result = Solution.solution(122);
+            Assert.Equal(221, result);
+        }
     }
 }
--- NXNCPR-X72-1/Solution.cs
+++ NXNCPR-X72-1/Solution.cs
@@ -8,5 +8,95 @@ namespace NXNCPR_X72_1
 {
     public class Solution
     {
+        public static int solution(int N)
+        {
+            var numberAsString = N.ToString();
+
+            var parsedCount = new Dictionary<int, int>()
+            {
+                {0, 0},
+                {1, 0},
+                {2, 0},
+                {3, 0},
+                {4, 0},
+                {5, 0},
+                {6, 0},
+                {7, 0},
+                {8, 0},
+                {9, 0}
+            };
+
+            foreach (var c in numberAsString)
+            {
+                var parsedDigit = Int32.Parse(c.ToString());
+                parsedCount[parsedDigit] += 1;
+            }
+
+            var sb = new System.Text.StringBuilder();
+
+            while (parsedCount[9] > 0)
+            {
+                sb.Append(9);
+                parsedCount[9] -= 1;
+            }
+
+            while (parsedCount[8] > 0)
+            {
+                sb.Append(8);
+                parsedCount[8] -= 1;
+            }
+
+            while (parsedCount[7] > 0)
+            {
+                sb.Append(7);
+                parsedCount[7] -= 1;
+            }
+
+            while (parsedCount[6] > 0)
+            {
+                sb.Append(6);
+                parsedCount[6] -= 1;
+            }
+
+            while (parsedCount[5] > 0)
+            {
+                sb.Append(5);
+                parsedCount[5] -= 1;
+            }
+
+            while (parsedCount[4] > 0)
+            {
+                sb.Append(4);
+                parsedCount[4] -= 1;
+            }
+
+            while (parsedCount[3] > 0)
+            {
+                sb.Append(3);
+                parsedCount[3] -= 1;
+            }
+
+            while (parsedCount[2] > 0)
+            {
+                sb.Append(2);
+                parsedCount[2] -= 1;
+            }
+
+            while (parsedCount[1] > 0)
+            {
+                sb.Append(1);
+                parsedCount[1] -= 1;
+            }
+
+            while (parsedCount[0] > 0)
+            {
+                sb.Append(0);
+                parsedCount[0] -= 1;
+            }
+
+            Console.Write(sb.ToString());
+
+            return Int32.Parse(sb.ToString());
+        }
     }
 }
