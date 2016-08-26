557499c16fd71c0d72539cf0101985a91877a05e
azam alias <azamalias@gmail.com>
2016-08-27 01:25:00 +0800

    added projects for Task1

---
+++ NXNCPR-X72-1.Tests/SolutionTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NXNCPR_X72_1.Tests
+{
+    public class SolutionTests
+    {
+    }
+}
